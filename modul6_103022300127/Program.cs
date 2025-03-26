2b53945309886c9da02553857b5e7eabd23ecd63
nicohzk <nicohzk05@gmail.com>
2025-03-26 07:09:22 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser.

--- modul6_103022300127/Program.cs
+++ modul6_103022300127/Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class SayaTubeUser {
+    private int id;
+    private List<SayaTubeVideo> uploadedVideos;
+    public string Username;
+
+    public SayaTubeUser(string a)
+    {
+        this.Username = a;
+        Random x = new Random();
+        this.id = x.Next(1, 99999);
+        uploadedVideos = new List<SayaTubeVideo>();
+    }
+
+    public int GetTotalVideoPlayCount() {
+        int x = 0;
+        for (int i = 0; i < uploadedVideos.Count; i++)
+        {
+            x += uploadedVideos[i].GetplayCount();
+        }
+        return x;
+    }
+    public void AddVideo(SayaTubeVideo o) {
+        uploadedVideos.Add(o);
+    }
+
+    public void PrintAllPlaycount() {
+        Console.WriteLine("Username: " + this.Username);
+        for (int i = 0; i < uploadedVideos.Count; i++) {
+            Console.WriteLine("Video " + (i+1) + ": ");
+            uploadedVideos[i].PrintVideoDetails();
+        }
+    }
+}
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playcount;
+
+    public SayaTubeVideo(string a)
+    {
+        this.title = a;
+        Random x = new Random();
+        id = x.Next(1, 99999);
+        this.playcount = 0;
+    }
+
+    public void IncreaseVideoPlayCount(int x)
+    {
+        this.playcount += x;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("id :" + this.id);
+        Console.WriteLine("title :" + this.title);
+        Console.WriteLine("playcount :" + this.playcount);
+    }
+    public int GetplayCount() {
+        return this.playcount;
+    }
+}
+class system
+{
+    public static void Main()
+    {
+        SayaTubeUser x = new SayaTubeUser("Hizkia");
+        SayaTubeVideo a = new SayaTubeVideo("Review Film 1 oleh Hizkia.");
+        SayaTubeVideo b = new SayaTubeVideo("Review Film 2 oleh Hizkia.");
+        SayaTubeVideo c = new SayaTubeVideo("Review Film 3 oleh Hizkia.");
+        SayaTubeVideo d = new SayaTubeVideo("Review Film 4 oleh Hizkia.");
+        SayaTubeVideo e = new SayaTubeVideo("Review Film 5 oleh Hizkia.");
+        SayaTubeVideo f = new SayaTubeVideo("Review Film 6 oleh Hizkia.");
+        SayaTubeVideo g = new SayaTubeVideo("Review Film 7 oleh Hizkia.");
+        SayaTubeVideo h = new SayaTubeVideo("Review Film 8 oleh Hizkia.");
+        SayaTubeVideo i = new SayaTubeVideo("Review Film 9 oleh Hizkia.");
+        SayaTubeVideo j = new SayaTubeVideo("Review Film 10 oleh Hizkia.");
+
+        x.AddVideo(a);
+        x.AddVideo(b);
+        x.AddVideo(c);
+        x.AddVideo(d);
+        x.AddVideo(e);
+        x.AddVideo(f);
+        x.AddVideo(g);
+        x.AddVideo(h);
+        x.AddVideo(i);
+        x.AddVideo(j);
+
+        a.IncreaseVideoPlayCount(100);
+        a.PrintVideoDetails();
+
+        x.PrintAllPlaycount();
+        x.GetTotalVideoPlayCount();
+    }
+}
--- modul6_103022300127/obj/Debug/net8.0/modul6_103022300127.AssemblyInfo.cs
+++ modul6_103022300127/obj/Debug/net8.0/modul6_103022300127.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul6_103022300127")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f627864471ab04e6bbfbe11f8fafc62e84afff76")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul6_103022300127")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul6_103022300127")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
